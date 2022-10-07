b8e246df4389abdc61e4c095916594ec217eeb1c
Jordan Patel <21124677+JordansGit@users.noreply.github.com>
2022-10-07 16:11:43 +0100

    changed the display of dice numbers to dice images

--- src/components/Die.js
+++ src/components/Die.js
@@ -6,7 +6,7 @@ export default function Die(props){
   }
   return (
     <div className="die" style={styles} onClick={props.holdDice}>
-      <h2 className="die-num">{props.value}</h2>
+      <img className="die-img" src={`../images/dice-${props.value}.png`}></img>
     </div>
   )
 }
